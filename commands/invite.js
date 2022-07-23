94e42aed4720c19abf392df52865b6870cbdc43c
devrusty <rguillory246@gmail.com>
2022-07-23 08:54:33 -0500

    created checkCommandParameterTypes

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,5 @@
-module.exports = (client, message, args, cmd) => {
+const checkCommandParameterTypes = require("../lib/methods/checkCommandParameterTypes")
 
+module.exports = (client, message, args, cmd) => {
+    const parameterChecks = checkCommandParameterTypes(client, message, args, cmd)
 }
