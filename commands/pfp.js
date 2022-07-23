6931452830de8891a6405e7ebdb711d9ec608266
devrusty <rguillory246@gmail.com>
2022-07-23 17:13:52 -0500

    changes to a!pfp

--- commands/pfp.js
+++ commands/pfp.js
@@ -1,19 +1,24 @@
+const DiscordJS = require("discord.js")
 const checkCommandParameterTypes = require("../lib/methods/checkCommandParameterTypes")
 const botConfig = require("../config.json")
 
 module.exports = {
     Name: "pfp",
     Usage: `${botConfig.prefix}pfp`,
-    Arguments: "",
+    Arguments: "<@user>",
     Type: "Utility",
     Permissions: [],
     Invoke(client, message, args, cmd) {
-        let member = message.mentions.members.first();
-        if (member) {
-            const avatar = member.user.displayAvatarURL();
-            return message.reply(avatar)
-        }
-  
-        return message.reply("Oops! You need to mention a user!")
+        const member = message.mentions.members.first();
+        if (!member) return message.reply("Oops! You need to mention a user!")
+
+        const avatar = member.user.displayAvatarURL();
+        const embed = new DiscordJS.EmbedBuilder()
+            .setTitle(`${member.user.username}'s Avatar`)
+            .setImage(avatar)
+
+        return message.reply({
+            embeds: [embed]
+        })
     }
 } 
