d8e64bea03c4eabedbed9db172ea75cbd2482739
Mason A. Dean <100883553+Graphing-wiz@users.noreply.github.com>
2022-07-23 17:40:19 -0400

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,11 @@
+const checkCommandParameterTypes = require("../lib/methods/checkCommandParameterTypes")
+const botConfig = require("../config.json") module.exports = {
+    Name: "test",
+    Usage: `${botConfig.prefix}test`,
+    Arguments: "",
+    Type: "Utility",
+    Permissions: [],
+    Invoke(client, message, args, cmd) {
+        message.channel.send(`is maxx bran smal?`)
+    }
+}
