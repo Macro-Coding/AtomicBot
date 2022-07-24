a90f96a5003c0e81cd1ec8d8eff112f55d5d3f5d
devrusty <rguillory246@gmail.com>
2022-07-24 09:24:24 -0500

    updated support command

--- commands/support.js
+++ commands/support.js
@@ -1,5 +1,6 @@
-const checkCommandParameterTypes = require("../lib/methods/checkCommandParameterTypes")
-const botConfig = require("../config.json")                                    
+const { EmbedBuilder } = require('discord.js');
+const botConfig = require("../config.json")
+
 module.exports = {
     Name: "support",
     Usage: `${botConfig.prefix}support`,
@@ -7,12 +8,13 @@ module.exports = {
     Type: "Utility",
     Permissions: [],
     Invoke(client, message, args, cmd) {
-        const { EmbedBuilder } = require('discord.js');
-        const exampleEmbed = new EmbedBuilder()
-        exampleEmbed.setTitle('Invite')
-        var size = client.guilds.cache.size
-        var parsedSize = parseInt(size)
-        exampleEmbed.setDescription('Hia wanna make me in ' + parsedSize + " servers? If so use this link: https://discord.com/api/oauth2/authorize?client_id=999512448736432189&permissions=8&scope=bot \n looking for the Support Server? here you go: https://discord.gg/SEP74J3QHw")
-        return message.channel.send({ embeds: [exampleEmbed] })
+        const supportEmbed = new EmbedBuilder()
+        supportEmbed.setTitle("Support")
+        supportEmbed.addFields(
+            { name: "Invite Atomic", value: `[Invite](${botConfig.invite})`, inline: true },
+            { name: "Support Server", value: "[Join](https://discord.gg/SEP74J3QHw)", inline: true }
+        )
+
+        return message.channel.send({ embeds: [supportEmbed] })
     }
-  }
+}
