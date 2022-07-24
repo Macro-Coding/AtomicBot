a0a19a5ec68151744b1a9510dfeb4994f90f38d0
devrusty <rguillory246@gmail.com>
2022-07-23 19:50:43 -0500

    fixed 8ball command

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,14 +2,13 @@ const checkCommandParameterTypes = require("../lib/methods/checkCommandParameter
 const botConfig = require("../config.json")
 
 module.exports = {
-    Name: "8ball",
-    Usage: `${botConfig.prefix}8ball`,
-    Arguments: "",
-    Type: "Utility",
-    Permissions: [],
-    Invoke(client, message, args, cmd) {
-        const replies = ["Yes, for sure!", "Yeah I think so", "idk", "No, god no", "Nope definitely not"] 
-        return message.reply(replies[Math.floor(Math.random() * arr.length)])
-    })
+  Name: "8ball",
+  Usage: `${botConfig.prefix}8ball`,
+  Arguments: "",
+  Type: "Utility",
+  Permissions: [],
+  Invoke(client, message, args, cmd) {
+    const replies = ["Yes, for sure!", "Yeah I think so", "idk", "No, god no", "Nope definitely not"]
+    return message.reply(replies[Math.floor(Math.random() * arr.length)])
   }
-} 
+}
