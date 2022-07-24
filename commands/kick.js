6f2a0fa9218947035bb8179184b2367cd5dfb5fa
devrusty <rguillory246@gmail.com>
2022-07-24 10:48:57 -0500

    fixed kick command

--- commands/kick.js
+++ commands/kick.js
@@ -12,9 +12,10 @@ module.exports = {
     Invoke(client, message, args, cmd) {
         const author = message.member
         const reason = args[2] || ""
-        if (!author.permissions.has(this.Permissions)) return message.channel.send("You do not have permission to use a!kick. Missing `KICK_MEMBERS`")
+        if (!author.permissions.has(this.Permissions))
+            return message.channel.send("You do not have permission to use a!kick. Missing `KICK_MEMBERS`")
 
-        const member = message.mentions.users.first()
+        const member = message.mentions.members.first()
         if (!member) return message.channel.send("❌ Please mention a user.")
 
         member.kick(reason)
