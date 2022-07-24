039b65a6457c5c45385ec51cc45c07cb46c2157d
devrusty <rguillory246@gmail.com>
2022-07-24 08:23:18 -0500

    created kick command

---
+++ commands/kick.js
@@ -0,0 +1,22 @@
+const DiscordJS = require("discord.js")
+const BotConfig = require("../config.json")
+
+module.exports = {
+    Name: "kick",
+    Usage: `${BotConfig.prefix}kick <user> <?reason>`,
+    Arguments: "<user> <?reason>",
+    Type: "Moderation",
+    Permissions: [
+        DiscordJS.PermissionFlagsBits.KickMembers
+    ],
+    Invoke(client, message, args, cmd) {
+        const author = message.member
+        const reason = args[2] || ""
+        if (!author.permissions.has(this.Permissions)) return message.channel.send("You do not have permission to use a!kick. Missing `KICK_MEMBERS`")
+
+        const member = message.mentions.users.first()
+        if (!member) return message.channel.send("❌ Please mention a user.")
+
+        member.kick(reason)
+    }
+}
