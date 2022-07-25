3d4bed4aeba3db299735407f5251ce41fec0fbd6
devrusty <rguillory246@gmail.com>
2022-07-24 20:34:36 -0500

    changes - added channelMentionToId

--- commands/announce.js
+++ commands/announce.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder } = require("discord.js")
 const botConfig = require("../config.json")
+const channelMentionToId = require("../lib/methods/channelMentionToId")
 
 module.exports = {
     Name: "announce",
@@ -8,17 +9,24 @@ module.exports = {
     Type: "Utility",
     Permissions: [],
     Invoke(client, message, args, cmd) {
-        const description = message.content.split("\\")
-        if (!title || description[1] || description[2])
-            return message.channel.send("the announcement must have a title a channel and description.")
+        const title = args[1]
+        const channel = args[2]
+        const description = args
+        if (!title || !channel || !description)
+            return message.channel.send("Announcement must have a title a channel and description.")
+
+        const TargetChannel = message.guild.channels.cache.get(channelMentionToId(channel))
+        console.log(TargetChannel.id)
+        if (!TargetChannel) return message.channel.send("Invalid channel.")
 
         const AnnEmbed = new EmbedBuilder()
             .setTitle(`Announcement - ${title}`)
             .setTimestamp()
+            .setColor("Aqua")
             .setDescription(`Created by ${message.author.tag} \n ${description[2]}`)
 
         message.delete() //Ann = abreviation for announcement
-        description[1].send({
+        return TargetChannel.send({
             embeds: [AnnEmbed]
         })
     }
