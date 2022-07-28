6d3a6b313d45c2583b7df010de3bdb771dbedd24
Mason A. Dean <100883553+Graphing-wiz@users.noreply.github.com>
2022-07-28 18:14:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,31 +9,28 @@ const Client = new DiscordJS.Client({ intents: BotConfig.intents })
 const Commands = new Object()
 
 /* Command handling */
-const CommandFiles = FS.readdirSync(Path.join(__dirname, "./commands/")).filter(file => file.endsWith(".js"))
-
-for (var file of CommandFiles) {
-    Commands[file.replace(".js", "")] = file.toLowerCase()
-}
-
+const CommandFiles = FS.readdirSync(Path.join(__dirname, "./commands/"))
+    .filter(file => file.endsWith(".js"))
+for (var file of CommandFiles) { Commands[file.replace(".js", "")] = file.toLowerCase() }
 Client.on("ready", () => {
     Client.user.setPresence({ activities: [{ name: "a!help" }], status: "online" });
     console.log(`${Client.user.username} is now online.`)
+}) Client.on("messageCreate", (message) => {
+    try {
+        if (message.author.bot) return
+        if (!message.content.startsWith(BotConfig.prefix)) return console.log(`${message.author.tag} in ${message.guild} in ${message.channel.name}: ${message.content}`)
+        const args = message.content.trim()
+            .split(/ +/g) const cmd = args[0].slice(BotConfig.prefix.length)
+                .toLowerCase()
+
+        const commandIndex = Commands[cmd]
+        if (!commandIndex) return message.channel.send(`Invalid command \`${cmd}\`. Please use \`${BotConfig.prefix}help\` for a list of commands.`)
+
+        const commandData = require(`./commands/${commandIndex}`)
+        commandData.Invoke(Client, message, args, cmd)
+    } catch (e) {
+        message.reply("Dang it! an error occurred! ❌")
+        console.log(e)
+    }
 })
-
-Client.on("messageCreate", (message) => {
-    if (message.author.bot) return
-    if (!message.content.startsWith(BotConfig.prefix)) return
-
-    const args = message.content.trim().split(/ +/g)
-    const cmd = args[0].slice(BotConfig.prefix.length).toLowerCase()
-
-    const commandIndex = Commands[cmd]
-
-    if (!commandIndex)
-        return message.channel.send(`Invalid command \`${cmd}\`. Please use \`${BotConfig.prefix}help\` for a list of commands.`)
-
-    const commandData = require(`./commands/${commandIndex}`)
-    commandData.Invoke(Client, message, args, cmd)
-})
-
-Client.login(BotConfig.token)
+Client.login(BotConfig.token) 
